5015ed0c2558ded9996a563baa3dddb4b79262ce
brite222 <brighttosin222@gmail.com>
2026-06-22 14:44:01 +0100

    Video Update

--- hybrrrr/src/pages/VideoLessonPage.jsx
+++ hybrrrr/src/pages/VideoLessonPage.jsx
@@ -5,6 +5,7 @@ import { awardPoints } from "../utils/awardPoints";
 import StudentSidebar from "../components/StudentSidebar";
 import "../styles/module.css";
 import "../styles/video-lesson.css";
+import alphaLogo from "../assets/images/alpha-loggo.png";
 
 const ChevronDown = () => (<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><polyline points="6 9 12 15 18 9" /></svg>);
 const ArrowLeft = () => (<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="19" y1="12" x2="5" y2="12" /><polyline points="12 19 5 12 12 5" /></svg>);
